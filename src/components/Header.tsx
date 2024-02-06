2d9464e1b80908bcd2ff8821f7b92e179508f510
Ezz dghedy <ezzmohsend@gmail.com>
2024-02-06 16:46:02 +0200

    starting useReducer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
+import { useGlobalContext } from '../store/timers-context.tsx';
 import Button from './UI/Button.tsx';
 
 export default function Header() {
+  const timerCtx = useGlobalContext();
+  timerCtx;
   return (
     <header>
       <h1>ReactTimer</h1>
 
-      <Button>Stop Timers</Button>
+      <Button>{timerCtx.isRunning ? 'stop Timers' : 'start Timers'}</Button>
     </header>
   );
 }
--- src/store/timers-context.tsx
+++ src/store/timers-context.tsx
@@ -9,24 +9,39 @@ type TimersState = {
   isRunning: boolean;
   timers: Timer[];
 };
+const initialState: TimersState = {
+  isRunning: false,
+  timers: [],
+};
 type TimersContextValue = TimersState & {
   addTimer: (timerData: Timer) => void;
   startTimers: () => void;
   stopTimers: () => void;
 };
+type Action = {
+  type: 'ADD_TIMERS' | 'START_TIMERS' | 'STOP_TIMERS';
+};
 type TimeProps = {
   children: React.ReactNode;
 };
+const timerReducer = (state: TimersState, action: Action): TimersState => {};
 
 const TimersContext = React.createContext<TimersContextValue | null>(null);
-
+export const useGlobalContext = () => React.useContext(TimersContext)!;
 const TimerContextProvider: React.FC<TimeProps> = ({ children }) => {
+  const [timerState, dispatch] = React.useReducer(reducer, initialState);
   const ctx: TimersContextValue = {
     isRunning: false,
     timers: [],
-    addTimer(timerData) {},
-    startTimers() {},
-    stopTimers() {},
+    addTimer(timerData) {
+      dispatch({ type: 'ADD_TIMERS' });
+    },
+    startTimers() {
+      dispatch({ type: 'START_TIMERS' });
+    },
+    stopTimers() {
+      dispatch({ type: 'START_TIMERS' });
+    },
   };
   return (
     <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
