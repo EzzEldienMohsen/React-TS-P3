c969015431d1e9664bef1cbaff8d26c44e1d0e88
Ezz dghedy <ezzmohsend@gmail.com>
2024-02-06 16:00:34 +0200

    finishing the context

--- src/store/timers-context.tsx
+++ src/store/timers-context.tsx
@@ -14,5 +14,23 @@ type TimersContextValue = TimersState & {
   startTimers: () => void;
   stopTimers: () => void;
 };
+type TimeProps = {
+  children: React.ReactNode;
+};
 
 const TimersContext = React.createContext<TimersContextValue | null>(null);
+
+const TimerContextProvider: React.FC<TimeProps> = ({ children }) => {
+  const ctx: TimersContextValue = {
+    isRunning: false,
+    timers: [],
+    addTimer(timerData) {},
+    startTimers() {},
+    stopTimers() {},
+  };
+  return (
+    <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
+  );
+};
+
+export default TimerContextProvider;
