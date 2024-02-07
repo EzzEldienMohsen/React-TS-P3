e1e9003bbaf7d329230381e3c9b8bc14fb7ed97a
Ezz dghedy <ezzmohsend@gmail.com>
2024-02-07 12:11:04 +0200

    branch start

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -2,6 +2,8 @@ import { type Timer as TimerProps } from '../store/timers-context.tsx';
 import Container from './UI/Container.tsx';
 
 export default function Timer({ name, duration }: TimerProps) {
+  React.useState(duration * 1000);
+  setInterval(() => {}, 50);
   return (
     <Container as="article">
       <h2>{name}</h2>
