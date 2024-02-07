84ab06faed9e5c1cb66d4e43ee13b24a4a773c68
Ezz dghedy <ezzmohsend@gmail.com>
2024-02-07 13:21:29 +0200

    useEffect with typescript useState deeper understanding progrss tag

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,13 +1,22 @@
+import React from 'react';
 import { type Timer as TimerProps } from '../store/timers-context.tsx';
 import Container from './UI/Container.tsx';
 
 export default function Timer({ name, duration }: TimerProps) {
-  React.useState(duration * 1000);
-  setInterval(() => {}, 50);
+  const [remainingTime, setRemainingTime] = React.useState<number>(
+    duration * 1000
+  );
+  React.useEffect(() => {
+    setInterval(() => {
+      setRemainingTime((a) => a - 50);
+    }, 50);
+  }, []);
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
     </Container>
   );
 }
