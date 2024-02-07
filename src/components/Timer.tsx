b878aea7b4df22b609987adb49c9b72c670a59e6
Ezz dghedy <ezzmohsend@gmail.com>
2024-02-07 13:59:20 +0200

    useEffect with typescript finalizing

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -3,19 +3,27 @@ import { type Timer as TimerProps } from '../store/timers-context.tsx';
 import Container from './UI/Container.tsx';
 
 export default function Timer({ name, duration }: TimerProps) {
+  const interval = React.useRef<number | null>(null);
   const [remainingTime, setRemainingTime] = React.useState<number>(
     duration * 1000
   );
+  if (remainingTime <= 0) {
+    clearInterval(interval.current!);
+  }
   React.useEffect(() => {
-    setInterval(() => {
+    const timer = setInterval(() => {
       setRemainingTime((a) => a - 50);
     }, 50);
+    interval.current = timer;
+    return () => clearInterval(timer);
   }, []);
+  const formattedRemainingTime: string = (remainingTime / 1000).toFixed(0);
   return (
     <Container as="article">
       <h2>{name}</h2>
       <p>
         <progress max={duration * 1000} value={remainingTime} />
+        <p>{`${formattedRemainingTime} sec`}</p>
       </p>
     </Container>
   );
